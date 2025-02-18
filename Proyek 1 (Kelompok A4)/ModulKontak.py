e1741e51f8e0a35b89cab063bce42dfabb4d2f78
Dino <mdidermwan@gmail.com>
2025-02-19 03:43:01 +0700

    Update ModulKontak.py

--- Proyek 1 (Kelompok A4)/ModulKontak.py
+++ Proyek 1 (Kelompok A4)/ModulKontak.py
@@ -36,4 +36,59 @@ def CariKontak(Nama):
     else:
         print("Kontak tidak ditemukan.")
     
+import os
+
+fileKontak = "daftarkontak.txt"
+
+# Rangga Muhamad fajar
+def TambahKontak(Nama, Nomor):
+    template = f"\n Nama : {Nama}, Nomor : {Nomor}"
+    Buka = open("daftarkontak.txt","a")
+    Buka.write(template)
     
+# Zaidan Zulkaisi Setiaji
+def TampilKontak():
+    Buka = open("daftarkontak.txt", "r")
+    print(Buka.read())
+    Buka.close()
+
+# Nauval Khairiyan
+def MuatKontak():
+    if not os.path.exists(fileKontak):
+        return []
+    with open(fileKontak, "r") as file:
+        contacts = file.readlines()
+    return [contact.strip().split(",") for contact in contacts]
+
+def CariKontak(Nama):
+    contacts = MuatKontak()
+    hasil = []
+    for contact in contacts:
+        if Nama.lower() in contact[0].lower():
+            hasil.append(contact)
+
+    if hasil:
+        print("\n Hasil Pencarian:")
+        for contact in hasil:
+            print(f"{contact[0]} - {contact[1]}")
+    else:
+        print("Kontak tidak ditemukan.")
+
+# Muhamad Dino Dermawan
+def HapusKontak(NamaHapus):
+    if not os.path.exists("daftarkontak.txt"):
+        print("Tidak ada kontak yang tersimpan")
+        return
+    with open("daftarkontak.txt", "r") as file:
+        lines = file.readlines()
+    with open("daftarkontak.txt", "w") as file:
+        kontak_dihapus = False
+        for line in lines:
+            if NamaHapus.lower() not in line.lower():
+                file.write(line)
+            else:
+                kontak_dihapus = True
+    if kontak_dihapus:
+        print(f"kontak '{NamaHapus}' berhasil dihapus")
+    else:
+        print(f"kontak '{NamaHapus}' tidak ditemukan")
